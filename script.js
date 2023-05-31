fec0949063d10047b6eb6b0f6d3648e882ff33bd
Tallyson Freitas <tallyson.eu@outlook.com>
2023-05-31 20:51:05 -0300

    comecando a utilizar js

---
+++ script.js
@@ -0,0 +1,11 @@
+let menor = document.querySelector("input#menorid")
+let maior = document.querySelector("input#maiorid")
+let resultado = document.getElementById
+
+if(maiorque(maior > menor)){
+
+}
+
+function maiorque(a,b){
+    return a > b
+}
