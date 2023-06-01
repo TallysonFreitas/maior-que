635625f6b99cfcadf0d975e3c77fffdb49fa21de
Tallyson Freitas <tallyson.eu@outlook.com>
2023-05-31 21:03:50 -0300

    js incompleto

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let menor = document.querySelector("input#menorid")
 let maior = document.querySelector("input#maiorid")
-let resultado = document.getElementById
-
-if(maiorque(maior > menor)){
-
-}
+let resultado = document.getElementById("valores")
 
 function maiorque(a,b){
     return a > b
+}
+
+function validar(maior,menor){
+    document.getElementById('valores').style.display = "flex"
+    if(maiorque(maior > menor)){
+        resultado.innerHTML = ` o numero ${maior} é maior que ${menor}!`
+    }else{
+        resultado.innerHTML = ` o numero ${maior} é menor ou igual a ${menor}!`
+
+    }
 }
